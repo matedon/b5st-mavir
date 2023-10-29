2e9f4f73fe4bc42f8f59a8db3c78e0b31f4d3411
matedon <matedon@gmail.com>
2023-10-29 11:59:00 +0100

    update carousel

--- carousel.php
+++ carousel.php
@@ -8,24 +8,24 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="2000">
+                    <img src="https://lh3.googleusercontent.com/pw/ADCreHdEtwHlqlxCpYk3T3zzQoEJ4j-z-VQabn4ubYbu3hTQ0N-5e83dt4uS1mEnfCqVgaiVXi5EYdFqlnSXlErpx5kMs_DDdJvBPY4DbowuHoQo4rCq8gorURjP5NhQmdzviqgxvTEpCal0xkRsSp1JIMB8Ag=w1920-h640-s-no-gm" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Szövegírás és Korrektúrázás</h5>
+                        <p>Cikkek, blogcikkek, ajánlók, közösségimédia-posztok, illetve nyelvészeti, nyelvtudományi területen szakmai szövegek készítése.</p>
+                    </div>
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
                     <img src="https://lh3.googleusercontent.com/pw/ADCreHdrJGj2b6l3pMVhROAfu63T-WmZHCBxkMLg-ZuYqwVWwyiOrN_3191KZljzDK-K0W1JAs8nt963bq5vD38DUJMtLi_ORw9rHRb7bqlntDbam8IYrfkMQW-52LX5mXF6B1vOa3II_pmm1aWEGIku-FiTqA=w1920-h640-s-no-gm" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                        <h5>Adminisztratív segítségnyújtás</h5>
+                        <p>Honlapok tartalmainak kezelése, ügyfélszolgálati feladatok (email, telefon), foglalkoztatást elősegítő programokkal kapcsolatos pályázatok megírása.</p>
                     </div>
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
                     <img src="https://lh3.googleusercontent.com/pw/ADCreHdYtWWmH7JduDR8PFDXaAvicL-x7Y14d9vyaBEMdOMgqhl0yiv5oiDnIBXl2ddEJhE6WGyUGQiWBF4ASYBQQ9HqrRDSZPtItkZicfo--SLxDZ1k6tvTsWwOW9V1lz1wJcrk09j6VAuNCZLBEnAEH3v4DQ=w1920-h640-s-no-gm" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="https://lh3.googleusercontent.com/pw/ADCreHfWypj7T3DOraWiSFTrZwckbsqocdchHYx4X3zhaCO0FgHK2vlG_HpJdDfiR-Pd4vowRralBsMNsfsS_AmOXZ0eBBpxFuw90g6SvIFYdpRB2XV5S27WaaSNF6xHSebjZRv7jPBs5HTFYdGn6sgeY_YeDA=w1920-h640-s-no-gm" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                        <h5>Programszevezés</h5>
+                        <p>Kisebb kulturális programok szervezésében segítségnyújtás, kapcsolati hálózat kiépítése.</p>
                     </div>
                 </div>
             </div>
