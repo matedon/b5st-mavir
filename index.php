dbc57f22f2bb0afe15deec95a3aabb1c5360db86
matedon <matedon@gmail.com>
2023-11-01 14:36:34 +0100

    aside order xl

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <main id="site-main" class="b5st-index container">
   <div class="row">
-    <div class="<?=(is_active_sidebar('sidebar-widget-area') ? 'col-xl-8' : '')?> col-12">
+    <div class="col-12 <?=(is_active_sidebar('sidebar-widget-area') ? 'col-xl-8 order-1' : '')?>">
       <?php
         b5st_mainbody_start();
         get_template_part('loops/index-loop');
@@ -13,7 +13,7 @@
       ?>
     </div>
     <?php if(is_active_sidebar('sidebar-widget-area')): ?>
-    <aside class="col-xl-4 col-12 mt-4">
+    <aside id="site-aside" class="col-12 col-xl-4 order-0 order-xl-3">
       <?php dynamic_sidebar('sidebar-widget-area'); ?>
     </aside>
     <?php endif; ?>
