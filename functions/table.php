2bf0bca170fa2c0019354531575752e63e5eac5f
matedon <matedon@gmail.com>
2023-11-02 06:22:02 +0100

    table-responsive

--- functions/table.php
+++ functions/table.php
@@ -2,5 +2,7 @@
 add_filter( 'the_content', 'b5st_custom_table' );
 
 function b5st_custom_table($content) {
-    return str_replace( '<table>', '<table class="table table-striped">', $content );
+    $content = str_replace( '<table>', '<div class="table-responsive"><table class="table table-striped">', $content);
+    $content = str_replace( '</table>', '</table></div>', $content);
+    return $content;
 }
